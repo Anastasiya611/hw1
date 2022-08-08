a3205119776a15d31fc887a9d1e8a6619b4dde84
Anastasiya <asa611@mail.ru>
2022-08-08 20:18:13 +0300

    hw

---
+++ hw006/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+System.Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+if (numberA % 2 == 0)
+{
+Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}
---
+++ nw004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+System.Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите третье число: ");
+int numberС = int.Parse(Console.ReadLine()!);
+int max = numberA;
+if (numberA > max)
+{
+max = numberA;
+}
+if (numberB > max)
+{
+max = numberB;
+}
+if (numberС > max)
+{
+max = numberС;
+}
+Console.WriteLine (max);
+
